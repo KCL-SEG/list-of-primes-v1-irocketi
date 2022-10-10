efe15c974dac22d5242684602acd545b7628c0c9
Sabeeka Ahmad (k20012890) <sabeeka.1.ahmad@kcl.ac.uk>
2022-10-10 21:40:36 +0100

    Attempt initial solution

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
+
+def checkPrime(x):
+    if x == 2 or x == 3:
+        return True
+    for i in range(2, math.ceil(x/2)+1):
+        if x%i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
     list = []
+    x = 0
+    while len(list) <= number_of_primes+1:
+        isPrime = checkPrime(x)
+        if isPrime:
+            list.append(x)
+        x += 1
     return list
+
+primes(10)
